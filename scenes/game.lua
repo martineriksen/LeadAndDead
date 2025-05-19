6dd132541f3f9e1e9e237fc5fda8c1adf3d9cac1
martineriksen <martineriksen@gmail.com>
2025-05-19 23:35:46 +0200

    now a bit more restructured

--- scenes/game.lua
+++ scenes/game.lua
@@ -1,25 +1,30 @@
-local player = require 'entities.player'
-local map = require 'systems.map'
-local cam = require 'systems.camera'
+local scene = require 'libraries.scene'
+local playerModule = require 'entities.player'
+local sti = require 'libraries.sti'
 
 local game = {}
 
+local rootLayer, player, gameMap
+
 function game.load()
-    map.load()
-    player.load(map)
-    cam.load(player, map)
+    rootLayer = scene.newLayer()
+    gameMap = sti('maps/testMap.lua')
+    local playerImage = love.graphics.newImage('sprites/cowboy1.png')
+    local scale = gameMap.tilewidth / playerImage:getWidth()
+    player = playerModule.new(400, 200, playerImage, scale)
+    rootLayer:insertChild(player)
 end
 
 function game.update(dt)
-    player.update(dt)
-    cam.update()
+    player:update(dt)
 end
 
 function game.draw()
-    cam.attach()
-    map.draw()
-    player.draw()
-    cam.detach()
+    if gameMap then
+        gameMap:drawLayer(gameMap.layers["Ground"])
+        gameMap:drawLayer(gameMap.layers["Trees"])
+    end
+    rootLayer:draw()
 end
 
 return game
