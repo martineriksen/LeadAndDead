d1b7d07ba5de0058fca11258bbd33de0d478ea0d
martineriksen <martineriksen@gmail.com>
2025-05-19 17:36:31 +0200

    updates

--- main.lua
+++ main.lua
@@ -2,7 +2,6 @@ function love.load()
     camera = require 'libraries/camera'
     cam = camera()
 
-    anim8 = require 'libraries/anim8'
     love.graphics.setDefaultFilter("nearest", "nearest")
 
     sti = require 'libraries/sti'
@@ -12,53 +11,30 @@ function love.load()
     player.x = 400
     player.y = 200
     player.speed = 5
-    player.spriteSheet = love.graphics.newImage('sprites/player-sheet.png')
-    player.grid = anim8.newGrid( 12, 18, player.spriteSheet:getWidth(), player.spriteSheet:getHeight() )
-
-    player.animations = {}
-    player.animations.down = anim8.newAnimation( player.grid('1-4', 1), 0.2 )
-    player.animations.left = anim8.newAnimation( player.grid('1-4', 2), 0.2 )
-    player.animations.right = anim8.newAnimation( player.grid('1-4', 3), 0.2 )
-    player.animations.up = anim8.newAnimation( player.grid('1-4', 4), 0.2 )
-
-    player.anim = player.animations.left
-
     background = love.graphics.newImage('sprites/background.png')
 end
 
 function love.update(dt)
     local isMoving = false
 
-    if love.keyboard.isDown("right") then
+    if love.keyboard.isDown("d") then
         player.x = player.x + player.speed
-        player.anim = player.animations.right
         isMoving = true
     end
-
-    if love.keyboard.isDown("left") then
+    if love.keyboard.isDown("a") then
         player.x = player.x - player.speed
-        player.anim = player.animations.left
         isMoving = true
     end
 
-    if love.keyboard.isDown("down") then
+    if love.keyboard.isDown("s") then
         player.y = player.y + player.speed
-        player.anim = player.animations.down
         isMoving = true
     end
 
-    if love.keyboard.isDown("up") then
+    if love.keyboard.isDown("w") then
         player.y = player.y - player.speed
-        player.anim = player.animations.up
         isMoving = true
     end
-
-    if isMoving == false then
-        player.anim:gotoFrame(2)
-    end
-
-    player.anim:update(dt)
-
     -- Update camera position
     cam:lookAt(player.x, player.y)
 
@@ -68,13 +44,13 @@ function love.update(dt)
     local h = love.graphics.getHeight()
 
     -- Left border
-    if cam.x < w/2 then
-        cam.x = w/2
+    if cam.x < w / 2 then
+        cam.x = w / 2
     end
 
     -- Right border
-    if cam.y < h/2 then
-        cam.y = h/2
+    if cam.y < h / 2 then
+        cam.y = h / 2
     end
 
     -- Get width/height of background
@@ -82,19 +58,21 @@ function love.update(dt)
     local mapH = gameMap.height * gameMap.tileheight
 
     -- Right border
-    if cam.x > (mapW - w/2) then
-        cam.x = (mapW - w/2)
+    if cam.x > (mapW - w / 2) then
+        cam.x = (mapW - w / 2)
     end
     -- Bottom border
-    if cam.y > (mapH - h/2) then
-        cam.y = (mapH - h/2)
+    if cam.y > (mapH - h / 2) then
+        cam.y = (mapH - h / 2)
     end
 end
 
 function love.draw()
     cam:attach()
-        gameMap:drawLayer(gameMap.layers["Ground"])
-        gameMap:drawLayer(gameMap.layers["Trees"])
-        player.anim:draw(player.spriteSheet, player.x, player.y, nil, 6, nil, 6, 9)
+    gameMap:drawLayer(gameMap.layers["Ground"])
+    gameMap:drawLayer(gameMap.layers["Trees"])
+    local carImage = love.graphics.newImage('sprites/cowboy1.png')
+    local carScale = gameMap.tilewidth / carImage:getWidth()
+    love.graphics.draw(carImage, player.x, player.y, 0, carScale, carScale)
     cam:detach()
 end
